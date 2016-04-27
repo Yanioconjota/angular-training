20c628347c4e6853a2c132516e18c2b164ddb50b
Yanioconjota <janioisacura@gmail.com>
2016-04-26 23:55:48 -0300

    This is where the fun begins

---
+++ public/js/app.js
@@ -0,0 +1,81 @@
+var app = angular.module('exampleApp', ['ngAnimate','ui.bootstrap','ui.validate']);
+
+app.run(function($rootScope, $interval){
+	//para inspeccionar el scope acceder por batarang, seleccionar el scope y escribir $scope en la consola
+	// $rootScope.$$watchers[0].get
+	//accede al watcher del scope
+
+	$interval(function(){
+		$rootScope.now = new Date().toLocaleTimeString();
+		//Angular revisa la lista de watchers con el digest y los dispara
+		//$rootScope.$digest();
+	}, 1000);
+});
+
+app.controller('BatmanDetector', function($scope){
+	var vm = this;
+
+		var batmanDetector = function(){
+			if (vm.firstName === 'Batman' || vm.firstName === 'Superman'){
+				if (vm.firstName === 'Batman') {
+					vm.supers = 'label label-primary';
+				} else if (vm.firstName === 'Superman'){
+					vm.supers = 'label label-danger';
+				}
+
+				return vm.firstName;
+			}
+		};
+		$scope.$watch(batmanDetector,function(isBatman){
+			if(isBatman){
+				vm.sufix = 'y tu mamá se llama Martha ';
+			}
+			else {
+				vm.sufix = '';
+			}
+		});
+});
+
+app.controller('Validador',function($scope){
+	var vm = this;
+	vm.comprobar = 90;
+
+	vm.save = function(){
+		if(vm.form.$valid){
+			vm.saved = 'This form is valid';
+			setTimeout(function(){
+				vm.saved = ''
+			},
+			2000);
+		}
+	}
+
+	// Forma villera del amo y señor
+	// vm.validateInput = function(name, type) {
+	// 	var input = vm.form[name];
+	// 		return (input.$dirty || vm.submitted) && ( input.$error[type] || ! input.$valid );
+	// }
+
+});
+
+app.controller('myController', function($scope){
+	var vm = this;
+	vm.numberArray = [1,2,3,4,5];
+	vm.doSomething = function(number){
+		console.log(number);
+		return number;
+	}
+
+	vm.array = [
+		{id:'NES',firstName:'Freddy',lastName:'Krueger'},
+		{id:'F13',firstName:'Jason',lastName:'Voorhees'},
+		{id:'HWE',firstName:'Michael',lastName:'Myers'}
+	]
+
+	vm.add = function(obj){
+		console.log(obj);
+		//vm.array.push(angular.copy(obj));
+		vm.array.push({id:vm.array.length + 1,firstName:vm.newItem.firstName, lastName:vm.newItem.lastName});
+
+	}
+});
